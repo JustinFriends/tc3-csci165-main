23fb047a4615b42d83a3b8d44673348597fd6887
JustinFriends <friends.justin@gmail.com>
2020-01-29 21:22:13 -0500

    Submission for Lab 1

---
+++ lab-1/HelloWorld2.java
@@ -0,0 +1,10 @@
+// file name must match the public class identifier
+public class HelloWorld2{
+
+    //main method is the starting point of any Java app
+    public static void main(String[] args){
+
+    System.out.println("Hello " + (args[0]) + " " + (args[1]) + ". Nice work processing the arguments");
+
+    } //end of main
+} //end of class
