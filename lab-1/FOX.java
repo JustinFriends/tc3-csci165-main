e6c5a106ce329a7a04be533f5ed31b5e26080b3b
JustinFriends <friends.justin@gmail.com>
2020-01-31 09:09:30 -0500

    Clean up working directory

---
+++ lab-1/FOX.java
@@ -0,0 +1,33 @@
+// file name must match the public class identifier
+public class FOX{
+
+    //main method is the starting point of any Java app
+    public static void main(String[] args){
+
+    //for(int x = 6; x < 0; x = x - 1){
+    System.out.println("*****");
+    System.out.println("****");
+    System.out.println("***");
+    System.out.println("**");
+    System.out.println("*");
+
+    System.out.println();
+
+    System.out.println("*****");
+    System.out.println("*   *");
+    System.out.println("*   *");
+    System.out.println("*   *");
+    System.out.println("*****");
+
+    System.out.println();
+
+    System.out.println("*****");
+    System.out.println(" *** ");
+    System.out.println(" **  ");
+    System.out.println(" *** ");
+    System.out.println("*****");
+
+    //}
+
+    } //end of main
+} //end of class
